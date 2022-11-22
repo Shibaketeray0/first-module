a70e667090c244527c76d6f6eba5a20c4ef74df0
Denys <denishomov2015@gmail.com>
2022-11-22 14:39:33 +0200

    Custom module init

---
+++ web/modules/custom/den4ik/src/Controller/cats.php
@@ -0,0 +1,11 @@
+<?php
+namespace Drupal\den4ik\Controller;
+
+class cats {
+  public function content() {
+    return[
+      '#markup' => 'Hello! You can add here a photo of your cat.'
+    ];
+  }
+}
+
